a6b4047f3758e30e4d534163baa75f4ac5eb75fe
Budeestar <55889494+Budeestar@users.noreply.github.com>
2023-10-26 19:06:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randonNumber1=Math.floor(Math.random()*6)+1
-var diceimage="images/dice"+randonNumber1+".png"
+var diceimage="dice"+randonNumber1+".png"
 var img1=document.querySelectorAll("img")[0].setAttribute("src",diceimage)
 
 
 var randonNumber2=Math.floor(Math.random()*6)+1
-var diceimage2="images/dice"+randonNumber2+".png"
+var diceimage2="dice"+randonNumber2+".png"
 var img2=document.querySelectorAll("img")[1].setAttribute("src",diceimage2)
 
 if (randonNumber1>randonNumber2){
